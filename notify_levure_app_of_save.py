8a3afe09f13ed33ecc68cae51450c294c23647a1
Trevor DeVore <trevor@screensteps.com>
2016-12-17 13:10:16 -0600

    Add some comments

--- notify_levure_app_of_save.py
+++ notify_levure_app_of_save.py
@@ -6,12 +6,14 @@ class LevureAppNotify(sublime_plugin.EventListener):
     def on_post_save(self, view):
         # 1. Get script only stack name. line 1: script "Name" [done]
         # 2. Get project key from project settings
-        # 3. Send notification over socket with project key and script name
+        # 3. Send notification over socket with project key, script name, and filename
         # 4. Get response from LiveCode IDE
 
+        # We are only concerned with files using Livecode syntax
         if view.settings().get('syntax') == 'Packages/LiveCode/LiveCode.sublime-syntax':
             stack_name = None
 
+            # Get the script only stack name
             # \A matches beginning of file
             region = view.find('\Ascript "([-a-zA-Z0-9_\s\?!]+)"', 0, sublime.IGNORECASE)
             if region.a >= 0:
