b3e0203751f79ab4eda9ff1e8febab9b3b3d4886
Trevor DeVore <trevor@screensteps.com>
2016-12-19 14:27:45 -0600

    - Set `livecode`.`notify_on_save` to true in project settings file to notify server when file has been saved.
    "livecode":
    {
        "notify_on_save": true,
    }

--- notify_levure_app_of_save.py
+++ notify_levure_app_of_save.py
@@ -5,7 +5,7 @@ import socket
 import urllib
 import errno
 
-class LiveCode(sublime_plugin.EventListener):
+class LiveCodeNotifyOnSave(sublime_plugin.EventListener):
     def on_post_save(self, view):
         # 1. Get script only stack name. line 1: script "Name" [done]
         # 2. Get project key from project settings
@@ -13,32 +13,34 @@ class LiveCode(sublime_plugin.EventListener):
         # 4. Get response from LiveCode IDE
 
         # We are only concerned with files using Livecode syntax
-        if view.settings().get('syntax').endswith(self.__class__.__name__+'.sublime-syntax'):
-            stack_name = None
+        if view.settings().get('syntax').endswith('LiveCode.sublime-syntax'):
+            window_settings = view.window().project_data()
+            if window_settings.get('livecode').get('notify_on_save', False):
+                stack_name = None
 
-            # Get the script only stack name
-            # \A matches beginning of file
-            region = view.find('\Ascript "([-a-zA-Z0-9_\s\?!]+)"', 0, sublime.IGNORECASE)
-            if region.a >= 0:
-                stack_name = re.search('"([-a-zA-Z0-9_\s\?!]+)"', view.substr(region)).group(1)
+                # Get the script only stack name
+                # \A matches beginning of file
+                region = view.find('\Ascript "([-a-zA-Z0-9_\s\?!]+)"', 0, sublime.IGNORECASE)
+                if region.a >= 0:
+                    stack_name = re.search('"([-a-zA-Z0-9_\s\?!]+)"', view.substr(region)).group(1)
 
-                host ="localhost"
-                port = 62475
-                debug = False
+                    host ="localhost"
+                    port = 62475
+                    debug = False
 
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.SOCK_DGRAM
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.SOCK_DGRAM
 
-                try:
-                    s.connect((host,port))
-                    query = {'stack': stack_name, 'filename': view.file_name()}
-                    data = urllib.parse.urlencode(query) + "\n"
-                    s.send(data.encode())
-                    data = s.recv(1024).decode()
-                    s.close()
-                    if data != 'success':
-                        print('error updating script in LiveCode: ' + data)
-                    else:
-                        print('script updated in LiveCode')
-                except socket.error as exc:
-                    if debug:
-                        print(exc)
+                    try:
+                        s.connect((host,port))
+                        query = {'stack': stack_name, 'filename': view.file_name()}
+                        data = urllib.parse.urlencode(query) + "\n"
+                        s.send(data.encode())
+                        data = s.recv(1024).decode()
+                        s.close()
+                        if data != 'success':
+                            print('error updating script in LiveCode: ' + data)
+                        else:
+                            print('script updated in LiveCode')
+                    except socket.error as exc:
+                        if debug:
+                            print(exc)
